9fd5f2ceaf65d177bdbe1ccfe034660dc9b06a9d
Rayssa Cristina <rayssa.paulino92@gmail.com>
2020-04-07 00:06:55 -0300

    front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './global.css';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Nós O amamos, porque Ele nos amou primeiro. - 1 João 4.19
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />  
   );
 }
 
